0f7c41afa1bf498214c3c22b3c2084a0fe44a590
Moritz Deißner <moritz.deissner@turbinekreuzberg.com>
2020-12-01 09:28:22 +0100

    add git in php dockerimage

--- src/Client/GitClient.php
+++ src/Client/GitClient.php
@@ -4,5 +4,24 @@ namespace Workflow\Client;
 
 class GitClient
 {
+    /**
+     * @throws \Exception
+     *
+     * @return string
+     */
+    public function getTicketFromCurrentBranch(): string
+    {
+        $matches = [];
+        $branchName = $this->getCurrentBranchName();
+        preg_match("/(?'ticket'SUK-\d{3,5})/", $branchName, $matches);
+        if (!isset($matches['ticket'])) {
+            throw new \RuntimeException(sprintf('Ticket number not found in branch name %s', $branchName));
+        }
+        return $matches['ticket'];
+    }
 
+    private function getCurrentBranchName() : string
+    {
+        return exec('git rev-parse --abbrev-ref HEAD');
+    }
 }
--- tests/Unit/Client/GitClientTest.php
+++ tests/Unit/Client/GitClientTest.php
@@ -3,11 +3,17 @@
 namespace Unit\Client;
 
 use PHPUnit\Framework\TestCase;
+use Workflow\Client\GitClient;
 
 class GitClientTest extends TestCase
 {
     public function testGetTicketFromCurrentBranch(): void
     {
-
+        $client = new GitClient();
+        $testbranch = uniqid('testbranch', true);
+        exec('git switch -c '.$testbranch);
+        $ticket = $client->getTicketFromCurrentBranch();
+        exec('git branch -D '.$testbranch);
+        self::assertEquals('SUK-test', $ticket);
     }
 }
