03f3a7bd22a91651b2c7149b9ebf6c6870b9097b
Moritz Deißner <moritz.deissner@turbinekreuzberg.com>
2020-12-08 09:34:51 +0100

    remove git dependency from tests

--- tests/Unit/Workflow/TicketIdentifierTest.php
+++ tests/Unit/Workflow/TicketIdentifierTest.php
@@ -8,28 +8,12 @@ use Workflow\Workflow\TicketIdentifier;
 
 class TicketIdentifierTest extends TestCase
 {
-    private string $oldBranch;
 
-    protected function setUp(): void
+    public function testExtractTicketIdFromBranchName(): void
     {
-        $this->oldBranch = exec('git rev-parse --abbrev-ref HEAD');
-
-    }
-
-    protected function tearDown(): void
-    {
-        exec('git checkout ' . $this->oldBranch);
-    }
-
-    public function testExtractTicketIdFromCurrentBranch(): void
-    {
-        $gitClient = new GitClient();
         $ticketIdentifier = new TicketIdentifier();
         $testbranch = uniqid('BCM-100-', true);
-        exec('git checkout -b '.$testbranch);
-        $ticketIdentifier = $ticketIdentifier->extractFromBranchName($gitClient->getCurrentBranchName());
-        exec('git checkout '. $this->oldBranch );
-        exec('git branch -D '.$testbranch);
+        $ticketIdentifier = $ticketIdentifier->extractFromBranchName($testbranch);
         self::assertEquals('BCM-100', $ticketIdentifier);
     }
 }
