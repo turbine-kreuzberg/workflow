e486d77fc95d387e8b105fc69e14c202c6f054f9
Christian Müllenhagen <christian.muellenhagen@turbinekreuzberg.com>
2020-12-11 09:52:29 +0100

    add tests for client factory

--- tests/Unit/Workflow/ClientFactoryTest.php
+++ tests/Unit/Workflow/ClientFactoryTest.php
@@ -1,10 +1,10 @@
 <?php
-
 namespace Unit\Workflow;
 
 use PHPUnit\Framework\TestCase;
 use Workflow\Client\ClientFactory;
 use Workflow\Client\GitClient;
+use Workflow\Client\JiraClient;
 
 class ClientFactoryTest extends TestCase
 {
@@ -13,5 +13,12 @@ class ClientFactoryTest extends TestCase
         $clientFactory = new ClientFactory();
         $gitClient = $clientFactory->getGitClient();
         self::assertInstanceOf(GitClient::class, $gitClient);
+    }
+
+    public function testGetJiraClient(): void
+    {
+        $clientFactory = new ClientFactory();
+        $gitClient = $clientFactory->getJiraClient();
+        self::assertInstanceOf(JiraClient::class, $gitClient);
     }
 }
