f14d90d4e98c9df5ebe4a3774f69beffd2312964
benkuhn <ben.kuhn@turbinekreuzberg.com>
2020-12-15 09:44:17 +0100

    cover jiraClient get issue

--- tests/Unit/Client/JiraClientTest.php
+++ tests/Unit/Client/JiraClientTest.php
@@ -6,6 +6,7 @@ use PHPUnit\Framework\TestCase;
 use Workflow\Client\Http\AtlassianHttpClient;
 use Workflow\Client\JiraClient;
 use Workflow\Configuration;
+use Workflow\Transfers\JiraIssueTransfer;
 
 class JiraClientTest extends TestCase
 {
@@ -35,4 +36,35 @@ class JiraClientTest extends TestCase
         $jiraClient->bookTime('BCM-12', ['worklogEntry']);
     }
 
+    public function testGetIssueUsesHttpClientToCallJiraWorklogEndpoint(): void
+    {
+        $jiraHttpClientMock = $this->createMock(AtlassianHttpClient::class);
+        $jiraHttpClientMock->expects(self::once())
+            ->method('get')
+            ->with('https://jira.votum.info:7443/rest/api/latest/issue/BCM-12')
+            ->willReturn(
+                [
+                    'key' => 'BCM-12',
+                    'fields' => [
+                        'summary' => 'summary',
+                        'issuetype' => [
+                            'subtask' => false,
+                        ],
+                        'labels' => ['label'],
+                    ],
+                ]
+            );
+
+        $jiraClient = new JiraClient($jiraHttpClientMock, $this->createMock(Configuration::class));
+        $jiraIssueTransfer = $jiraClient->getIssue(issue: 'BCM-12');
+
+        $expectedJiraIssueTransfer = new JiraIssueTransfer();
+        $expectedJiraIssueTransfer->key = 'BCM-12';
+        $expectedJiraIssueTransfer->isSubTask = false;
+        $expectedJiraIssueTransfer->summary = 'summary';
+        $expectedJiraIssueTransfer->url = 'https://jira.votum.info:7443/browse/BCM-12';
+        $expectedJiraIssueTransfer->labels = ['label'];
+
+        self::assertEquals($expectedJiraIssueTransfer, $jiraIssueTransfer);
+    }
 }
