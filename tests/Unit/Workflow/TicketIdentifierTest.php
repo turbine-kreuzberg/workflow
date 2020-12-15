de52d3468ed7530ab19a137c869a5166c0ca8f21
benkuhn <ben.kuhn@turbinekreuzberg.com>
2020-12-15 09:15:15 +0100

    cover TicketIdentifier with more tests

--- tests/Unit/Workflow/TicketIdentifierTest.php
+++ tests/Unit/Workflow/TicketIdentifierTest.php
@@ -3,12 +3,12 @@
 namespace Unit\Workflow;
 
 use PHPUnit\Framework\TestCase;
+use RuntimeException;
 use Workflow\Configuration;
 use Workflow\Workflow\TicketIdentifier;
 
 class TicketIdentifierTest extends TestCase
 {
-
     public function testExtractTicketIdFromBranchName(): void
     {
         $configurationMock = $this->createMock(Configuration::class);
@@ -17,8 +17,21 @@ class TicketIdentifierTest extends TestCase
             ->with('JIRA_PROJECT_KEY')
             ->willReturn('BCM');
         $ticketIdentifier = new TicketIdentifier($configurationMock);
-        $testbranch = uniqid('BCM-100-', true);
-        $ticketIdentifier = $ticketIdentifier->extractFromBranchName($testbranch);
+        $testBranch = uniqid('BCM-100-', true);
+        $ticketIdentifier = $ticketIdentifier->extractFromBranchName($testBranch);
         self::assertEquals('BCM-100', $ticketIdentifier);
+    }
+
+    public function testTicketNumberNotFoundInBranchNameThrowsException(): void
+    {
+        $configurationMock = $this->createMock(Configuration::class);
+        $configurationMock->expects(self::once())
+            ->method('getConfiguration')
+            ->with('JIRA_PROJECT_KEY')
+            ->willReturn('unknown project key');
+        $ticketIdentifier = new TicketIdentifier($configurationMock);
+        $testBranch = uniqid('BCM-100-', true);
+        $this->expectException(RuntimeException::class);
+        $ticketIdentifier->extractFromBranchName($testBranch);
     }
 }
