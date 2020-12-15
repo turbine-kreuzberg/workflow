9ae1581c8952eab7e03df41df9516fc7ef9703a1
Christian Müllenhagen <christian.muellenhagen@turbinekreuzberg.com>
2020-12-15 09:23:40 +0100

    more test coverage on jira client

--- tests/Unit/Client/JiraClientTest.php
+++ tests/Unit/Client/JiraClientTest.php
@@ -9,7 +9,7 @@ use Workflow\Configuration;
 
 class JiraClientTest extends TestCase
 {
-    public function testGetWorklogUsesHttpClientToCallWorklogEndpoint(): void
+    public function testGetWorklogUsesHttpClientToCallJiraWorklogEndpoint(): void
     {
         $jiraHttpClientMock = $this->createMock(AtlassianHttpClient::class);
         $jiraHttpClientMock->expects(self::once())
@@ -21,6 +21,18 @@ class JiraClientTest extends TestCase
         $worklog = $jiraClient->getWorkLog('BCM-12');
 
         self::assertSame(['some-worklog'], $worklog);
+    }
+
+    public function testBookTimeUsesHttpClientToPostWorklogEntryToJira(): void
+    {
+        $jiraHttpClientMock = $this->createMock(AtlassianHttpClient::class);
+        $jiraHttpClientMock->expects(self::once())
+            ->method('post')
+            ->with('https://jira.votum.info:7443/rest/api/latest/issue/BCM-12/worklog')
+            ->willReturn(['some-worklog']);
+
+        $jiraClient = new JiraClient($jiraHttpClientMock, $this->createMock(Configuration::class));
+        $jiraClient->bookTime('BCM-12', ['worklogEntry']);
     }
 
 }
