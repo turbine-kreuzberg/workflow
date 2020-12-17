75382418b9f379a20ac35dbbacaa323cb828d35a
benkuhn <ben.kuhn@turbinekreuzberg.com>
2020-12-17 09:28:51 +0100

    test put and post from AtlassianHttpClient

--- tests/Unit/Client/Http/AtlassianHttpClientTest.php
+++ tests/Unit/Client/Http/AtlassianHttpClientTest.php
@@ -11,13 +11,13 @@ use Workflow\Configuration;
 
 class AtlassianHttpClientTest extends TestCase
 {
-    public function testInitAtlassianClientWithGuzzle(): void
+    public function testGetFunctionCallsGuzzleClient(): void
     {
         $streamMock = $this->createMock(StreamInterface::class);
         $streamMock->expects(self::once())
             ->method('getContents')
             ->willReturn('{}');
-        
+
         $responseMock = $this->createMock(ResponseInterface::class);
         $responseMock->expects(self::once())
             ->method('getBody')
@@ -43,5 +43,153 @@ class AtlassianHttpClientTest extends TestCase
         );
 
         self::assertEquals([], $atlassianHttpClient->get('url'));
+    }
+
+    public function testPostFunctionCallsGuzzleClient(): void
+    {
+        $streamMock = $this->createMock(StreamInterface::class);
+        $streamMock->expects(self::once())
+            ->method('getContents')
+            ->willReturn('{}');
+
+        $responseMock = $this->createMock(ResponseInterface::class);
+        $responseMock->expects(self::once())
+            ->method('getBody')
+            ->willReturn($streamMock);
+
+        $clientMock = $this->createMock(Client::class);
+        $clientMock->expects(self::once())
+            ->method('__call')
+            ->with(
+                'post',
+                ['url', ['json' => ['postOptions'], 'auth' => ['username', 'password']]]
+            )
+            ->willReturn($responseMock);
+
+        $configurationMock = $this->createMock(Configuration::class);
+        $configurationMock->expects(self::exactly(2))
+            ->method('getConfiguration')
+            ->withConsecutive(
+                ['JIRA_USERNAME'],
+                ['JIRA_PASSWORD'],
+            )
+            ->willReturnOnConsecutiveCalls('username', 'password');
+        $atlassianHttpClient = new AtlassianHttpClient(
+            $configurationMock,
+            $clientMock
+        );
+
+        self::assertEquals([], $atlassianHttpClient->post('url', ['postOptions']));
+    }
+
+    public function testPostFunctionWithInvalidJsonResponseReturnsAnEmptyArray(): void
+    {
+        $streamMock = $this->createMock(StreamInterface::class);
+        $streamMock->expects(self::once())
+            ->method('getContents')
+            ->willReturn('');
+
+        $responseMock = $this->createMock(ResponseInterface::class);
+        $responseMock->expects(self::once())
+            ->method('getBody')
+            ->willReturn($streamMock);
+
+        $clientMock = $this->createMock(Client::class);
+        $clientMock->expects(self::once())
+            ->method('__call')
+            ->with(
+                'post',
+                ['url', ['json' => ['postOptions'], 'auth' => ['username', 'password']]]
+            )
+            ->willReturn($responseMock);
+
+        $configurationMock = $this->createMock(Configuration::class);
+        $configurationMock->expects(self::exactly(2))
+            ->method('getConfiguration')
+            ->withConsecutive(
+                ['JIRA_USERNAME'],
+                ['JIRA_PASSWORD'],
+            )
+            ->willReturnOnConsecutiveCalls('username', 'password');
+        $atlassianHttpClient = new AtlassianHttpClient(
+            $configurationMock,
+            $clientMock
+        );
+
+        self::assertEquals([], $atlassianHttpClient->post('url', ['postOptions']));
+    }
+
+    public function testPutFunctionCallsGuzzleClient(): void
+    {
+        $streamMock = $this->createMock(StreamInterface::class);
+        $streamMock->expects(self::once())
+            ->method('getContents')
+            ->willReturn('{}');
+
+        $responseMock = $this->createMock(ResponseInterface::class);
+        $responseMock->expects(self::once())
+            ->method('getBody')
+            ->willReturn($streamMock);
+
+        $clientMock = $this->createMock(Client::class);
+        $clientMock->expects(self::once())
+            ->method('__call')
+            ->with(
+                'put',
+                ['url', ['json' => ['putOptions'], 'auth' => ['username', 'password']]]
+            )
+            ->willReturn($responseMock);
+
+        $configurationMock = $this->createMock(Configuration::class);
+        $configurationMock->expects(self::exactly(2))
+            ->method('getConfiguration')
+            ->withConsecutive(
+                ['JIRA_USERNAME'],
+                ['JIRA_PASSWORD'],
+            )
+            ->willReturnOnConsecutiveCalls('username', 'password');
+        $atlassianHttpClient = new AtlassianHttpClient(
+            $configurationMock,
+            $clientMock
+        );
+
+        self::assertEquals([], $atlassianHttpClient->put('url', ['putOptions']));
+    }
+
+    public function testPutFunctionWithInvalidJsonResponseReturnsAnEmptyArray(): void
+    {
+        $streamMock = $this->createMock(StreamInterface::class);
+        $streamMock->expects(self::once())
+            ->method('getContents')
+            ->willReturn('');
+
+        $responseMock = $this->createMock(ResponseInterface::class);
+        $responseMock->expects(self::once())
+            ->method('getBody')
+            ->willReturn($streamMock);
+
+        $clientMock = $this->createMock(Client::class);
+        $clientMock->expects(self::once())
+            ->method('__call')
+            ->with(
+                'put',
+                ['url', ['json' => ['putOptions'], 'auth' => ['username', 'password']]]
+            )
+            ->willReturn($responseMock);
+
+        $configurationMock = $this->createMock(Configuration::class);
+        $configurationMock->expects(self::exactly(2))
+            ->method('getConfiguration')
+            ->withConsecutive(
+                ['JIRA_USERNAME'],
+                ['JIRA_PASSWORD'],
+            )
+            ->willReturnOnConsecutiveCalls('username', 'password');
+        $atlassianHttpClient = new AtlassianHttpClient(
+            $configurationMock,
+            $clientMock
+        );
+
+        self::assertEquals([], $atlassianHttpClient->put('url', ['putOptions']));
     }
 }
