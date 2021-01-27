5f2aa51b03233c02d4ba37184d825266232eb2a0
Christian Müllenhagen <christian.muellenhagen@turbinekreuzberg.com>
2021-01-27 17:38:23 +0100

    Remove unneeded methods in JiraClient

--- src/Client/JiraClient.php
+++ src/Client/JiraClient.php
@@ -5,16 +5,13 @@ namespace Workflow\Client;
 use Exception;
 use Workflow\Client\Http\AtlassianHttpClient;
 use Workflow\Configuration;
-use Workflow\Exception\JiraStateNotFoundException;
 use Workflow\Transfers\JiraIssueTransfer;
-use Workflow\Transfers\JiraIssueTransferCollection;
 use Workflow\Workflow\Jira\Mapper\JiraIssueMapper;
 
 class JiraClient
 {
     private const BASE_URL = 'https://jira.votum.info:7443/';
     private const API_URL = self::BASE_URL . 'rest/api/latest/';
-    private const SEARCH_URL = self::API_URL . 'search/';
     private const BROWSE_URL = self::BASE_URL . 'browse/';
     private const ISSUE_URL = self::API_URL . 'issue/';
     private const BOARD_URL = self::BASE_URL . 'rest/agile/1.0/board/';
@@ -32,19 +29,6 @@ class JiraClient
         $responseArray = $this->jiraHttpClient->post(self::ISSUE_URL, $issueData);
 
         return $this->getIssue($responseArray['key']);
-    }
-
-    public function getCurrentIssuesInStatus(string $statusName): JiraIssueTransferCollection
-    {
-        $issueData = [
-            'jql' => 'project = ' . $this->getProjectName() . ' AND status="' . $statusName . '"',
-            'startAt' => 0,
-            'fields' => ['id', 'key', 'summary', 'issuetype'],
-        ];
-
-        $responseArray = $this->jiraHttpClient->post(self::SEARCH_URL, $issueData);
-
-        return $this->mapResponseToJiraIssueTransferCollection($responseArray);
     }
 
     public function getActiveSprint(): array
@@ -84,12 +68,6 @@ class JiraClient
         return $this->configuration->getConfiguration(Configuration::JIRA_USERNAME);
     }
 
-    public function getUserLocale(): string
-    {
-        $localeCall = self::API_URL . 'myself';
-        return $this->jiraHttpClient->get($localeCall)['locale'];
-    }
-
     public function bookTime(string $issue, array $worklogEntry): void
     {
         $bookTimeCall = self::ISSUE_URL . $issue . '/worklog';
@@ -112,27 +90,12 @@ class JiraClient
         );
     }
 
-    private function mapResponseToJiraIssueTransferCollection(array $responseArray): JiraIssueTransferCollection
-    {
-        $jiraIssues = [];
-        foreach ($responseArray['issues'] as $issue) {
-            $jiraIssues[] = $this->mapResponseToJiraIssueTransfer($issue);
-        }
-
-        return new JiraIssueTransferCollection($jiraIssues);
-    }
-
     private function mapResponseToJiraIssueTransfer(array $issue): JiraIssueTransfer
     {
         $jiraIssueTransfer = $this->jiraIssueMapper->map($issue);
         $jiraIssueTransfer->url = self::BROWSE_URL . $jiraIssueTransfer->key;
 
         return $jiraIssueTransfer;
-    }
-
-    private function getProjectName(): string
-    {
-        return $this->configuration->getConfiguration(Configuration::PROJECT_NAME);
     }
 
     private function getBoardId(): string
