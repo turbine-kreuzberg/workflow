1df0cf7afe096ffa461598ed60ad2eda1357058b
Özkan Yilmaz <oezkan.yilmaz@turbinekreuzberg.com>
2021-01-27 10:01:48 +0100

    add testCreateMergeRequestFailedThrowsException for GitlabClient

--- tests/Unit/Client/GitlabClientTest.php
+++ tests/Unit/Client/GitlabClientTest.php
@@ -2,6 +2,7 @@
 
 namespace Unit\Client;
 
+use GuzzleHttp\Exception\BadResponseException;
 use PHPUnit\Framework\TestCase;
 use Workflow\Client\GitlabClient;
 use Workflow\Client\Http\GitlabHttpClient;
@@ -55,5 +56,56 @@ class GitlabClientTest extends TestCase
         );
 
         self::assertEquals('url', $url);
+    }
+
+
+    public function testCreateMergeRequestFailedThrowsException(): void
+    {
+        $configurationMock = $this->createMock(Configuration::class);
+        $configurationMock->expects(self::exactly(3))
+            ->method('getConfiguration')
+            ->withConsecutive(
+                ['GITLAB_API_URL'],
+                ['REPOSITORY'],
+                ['BRANCH_DEVELOPMENT'],
+            )
+            ->willReturnOnConsecutiveCalls(
+                'GITLAB_API_URL',
+                'Project repository',
+                'BRANCH_DEVELOPMENT',
+            );
+        $configurationMock->expects(self::once())
+            ->method('getNonRemovableBranches')
+            ->willReturn([]);
+
+        $exceptionMock = $this->createMock(BadResponseException::class);
+
+        $gitlabHttpClientMock = $this->createMock(GitlabHttpClient::class);
+        $gitlabHttpClientMock->expects(self::once())
+            ->method('post')
+            ->with(
+                'GITLAB_API_URLprojects/Project+repository/merge_requests',
+                [
+                    'source_branch' => 'source_branch',
+                    'target_branch' => 'target_branch',
+                    'approvals_before_merge' => 2,
+                    'remove_source_branch' => true,
+                ]
+            )
+            ->willThrowException($exceptionMock);
+
+        $gitlabClient = new GitlabClient(
+            $gitlabHttpClientMock,
+            $configurationMock
+        );
+
+        $this->expectException(BadResponseException::class);
+
+        $gitlabClient->createMergeRequest(
+            [
+                'source_branch' => 'source_branch',
+                'target_branch' => 'target_branch',
+            ]
+        );
     }
 }
