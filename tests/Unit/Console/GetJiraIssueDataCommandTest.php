0201df54c0b2be5bc0594f809f4f782e748bd231
benkuhn <ben.kuhn@turbinekreuzberg.com>
2021-02-17 11:27:22 +0100

    more test for GetJiraIssueDataCommand

--- tests/Unit/Console/GetJiraIssueDataCommandTest.php
+++ tests/Unit/Console/GetJiraIssueDataCommandTest.php
@@ -111,6 +111,105 @@ class GetJiraIssueDataCommandTest extends TestCase
         $getJiraIssueDataCommand->run($inputMock, $outputMock);
     }
 
+    public function testGetTicketDataForTicketWithSubTasks(): void
+    {
+        $testJiraIssueTransfer = $this->createDummyJiraIssueTransfer();
+        $testJiraIssueTransfer->subTasks = [
+            [
+                'key' => 'key',
+                'fields' => [
+                    'summary' => 'summary',
+                    'status' => [
+                        'name' => 'name',
+                    ],
+                ],
+            ],
+        ];
+
+        $symfonyStyleMock = $this->createMock(SymfonyStyle::class);
+        $symfonyStyleMock->expects(self::exactly(2))
+            ->method('ask')
+            ->with('Ticket number')
+            ->willReturnOnConsecutiveCalls('99999', '12345');
+
+        $issueReaderMock = $this->createMock(IssueReader::class);
+        $issueReaderMock->expects(self::exactly(2))
+            ->method('getIssue')
+            ->willReturnOnConsecutiveCalls(self::throwException(new \Exception()), $testJiraIssueTransfer);
+
+        $workflowFactoryMock = $this->createMock(WorkflowFactory::class);
+        $workflowFactoryMock->expects(self::once())
+            ->method('createSymfonyStyle')
+            ->willReturn($symfonyStyleMock);
+
+        $getJiraIssueDataCommand = new GetJiraIssueDataCommand(
+            workflowFactory: $workflowFactoryMock,
+            issueReader: $issueReaderMock
+        );
+
+        $inputMock = $this->createMock(InputInterface::class);
+        $outputMock = $this->createMock(OutputInterface::class);
+
+        $getJiraIssueDataCommand->run($inputMock, $outputMock);
+    }
+
+    /**
+     * @dataProvider parentTypeProvider
+     *
+     * @param string|null $parentType
+     *
+     * @return void
+     */
+    public function testGetTicketDataSubTaskWithDifferentTypes(?string $parentType): void
+    {
+        $testJiraIssueTransfer = $this->createDummyJiraIssueTransfer();
+        $testJiraIssueTransfer->isSubTask = true;
+        $testJiraIssueTransfer->parentIssueType = $parentType;
+        $testJiraIssueTransfer->parentIssueKey = 'ABC-134';
+        $testJiraIssueTransfer->parentIssueSummary = 'parent summary';
+
+        $symfonyStyleMock = $this->createMock(SymfonyStyle::class);
+        $symfonyStyleMock->expects(self::never())
+            ->method('ask');
+
+        $issueReaderMock = $this->createMock(IssueReader::class);
+        $issueReaderMock->expects(self::once())
+            ->method('getIssue')
+            ->with('12345')
+            ->willReturn($testJiraIssueTransfer);
+
+        $workflowFactoryMock = $this->createMock(WorkflowFactory::class);
+        $workflowFactoryMock->expects(self::once())
+            ->method('createSymfonyStyle')
+            ->willReturn($symfonyStyleMock);
+
+        $getJiraIssueDataCommand = new GetJiraIssueDataCommand(
+            workflowFactory: $workflowFactoryMock,
+            issueReader: $issueReaderMock
+        );
+
+        $inputMock = $this->createMock(InputInterface::class);
+        $inputMock->expects(self::once())
+            ->method('getArgument')
+            ->with('ticket number')
+            ->willReturn('12345');
+
+        $outputMock = $this->createMock(OutputInterface::class);
+
+        $getJiraIssueDataCommand->run($inputMock, $outputMock);
+    }
+
+    public function parentTypeProvider(): array
+    {
+        return [
+            'not known type' => ['randomtype'],
+            'bug type' => ['bug'],
+            'story type' => ['story'],
+            'sub task type' => ['sub-task'],
+            'no type' => [null],
+        ];
+    }
+
     private function createDummyJiraIssueTransfer(): JiraIssueTransfer
     {
         $testJiraIssueTransfer = new JiraIssueTransfer();
