4d4350c354ec07b64eeeaf9b80a0640d12481c0c
Christian Müllenhagen <christian.muellenhagen@turbinekreuzberg.com>
2021-02-19 09:57:14 +0100

    Add missing tests for GitlabClient.php

--- tests/Unit/Client/GitlabClientTest.php
+++ tests/Unit/Client/GitlabClientTest.php
@@ -59,6 +59,102 @@ class GitlabClientTest extends TestCase
         self::assertEquals('url', $url);
     }
 
+    public function testCreateMergeRequestWithNonRemovableSourceBranch(): void
+    {
+        $configurationMock = $this->createMock(Configuration::class);
+        $configurationMock->expects(self::exactly(3))
+            ->method('get')
+            ->withConsecutive(
+                ['GITLAB_API_URL'],
+                ['REPOSITORY'],
+                ['BRANCH_DEVELOPMENT'],
+            )
+            ->willReturnOnConsecutiveCalls(
+                'GITLAB_API_URL',
+                'Project repository',
+                'BRANCH_DEVELOPMENT',
+            );
+        $configurationMock->expects(self::once())
+            ->method('getNonRemovableBranches')
+            ->willReturn(['source_branch']);
+
+        $gitlabHttpClientMock = $this->createMock(GitlabHttpClient::class);
+        $gitlabHttpClientMock->expects(self::once())
+            ->method('post')
+            ->with(
+                'GITLAB_API_URLprojects/Project+repository/merge_requests',
+                [
+                    'source_branch' => 'source_branch',
+                    'target_branch' => 'target_branch',
+                    'approvals_before_merge' => 2,
+                    'remove_source_branch' => false,
+                ]
+            )
+            ->willReturn(['web_url' => 'url']);
+
+        $gitlabClient = new GitlabClient(
+            $gitlabHttpClientMock,
+            $configurationMock
+        );
+
+        $url = $gitlabClient->createMergeRequest(
+            [
+                'source_branch' => 'source_branch',
+                'target_branch' => 'target_branch',
+            ]
+        );
+
+        self::assertEquals('url', $url);
+    }
+    public function testCreateMergeRequestAgainstDeploymentBranchDoesNotNeedApproval(): void
+    {
+        $configurationMock = $this->createMock(Configuration::class);
+        $configurationMock->expects(self::exactly(4))
+            ->method('get')
+            ->withConsecutive(
+                ['GITLAB_API_URL'],
+                ['REPOSITORY'],
+                ['BRANCH_DEVELOPMENT'],
+                ['BRANCH_DEPLOYMENT'],
+            )
+            ->willReturnOnConsecutiveCalls(
+                'GITLAB_API_URL',
+                'Project repository',
+                'BRANCH_DEVELOPMENT',
+                'BRANCH_DEPLOYMENT',
+            );
+        $configurationMock->expects(self::once())
+            ->method('getNonRemovableBranches')
+            ->willReturn([]);
+
+        $gitlabHttpClientMock = $this->createMock(GitlabHttpClient::class);
+        $gitlabHttpClientMock->expects(self::once())
+            ->method('post')
+            ->with(
+                'GITLAB_API_URLprojects/Project+repository/merge_requests',
+                [
+                    'source_branch' => 'BRANCH_DEVELOPMENT',
+                    'target_branch' => 'BRANCH_DEPLOYMENT',
+                    'approvals_before_merge' => 0,
+                    'remove_source_branch' => true,
+                ]
+            )
+            ->willReturn(['web_url' => 'url']);
+
+        $gitlabClient = new GitlabClient(
+            $gitlabHttpClientMock,
+            $configurationMock
+        );
+
+        $url = $gitlabClient->createMergeRequest(
+            [
+                'source_branch' => 'BRANCH_DEVELOPMENT',
+                'target_branch' => 'BRANCH_DEPLOYMENT',
+            ]
+        );
+
+        self::assertEquals('url', $url);
+    }
 
     public function testCreateMergeRequestFailedThrowsException(): void
     {
